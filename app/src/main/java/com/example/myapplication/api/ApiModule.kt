f061dec4b7ffda70f457779e9839a84c1376907e
Sergey Nikitin <kexa76@gmail.com>
2022-04-13 13:59:03 +0500

    quick fix

--- app/src/main/java/com/example/myapplication/api/ApiModule.kt
+++ app/src/main/java/com/example/myapplication/api/ApiModule.kt
@@ -1,8 +1,5 @@
 package com.example.myapplication.api
 
-import com.example.myapplication.dto.ImageDeserializer
-import com.example.myapplication.dto.Images
-import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,8 +18,6 @@ class ApiModule {
         private const val BASE_URL = "https://api.jikan.moe/v4/"
     }
 
-    var gson = GsonBuilder().registerTypeAdapter(Images::class.java, ImageDeserializer())
-        .create()
 
     @Singleton
     @Provides
@@ -33,7 +28,7 @@ class ApiModule {
     @Singleton
     @Provides
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create(gson))
+        .addConverterFactory(GsonConverterFactory.create())
         .baseUrl(BASE_URL)
         .client(okHttpClient)
         .build()
